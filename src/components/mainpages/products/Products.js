67b0ff813dcf596657f126c07f23070d6ccda3fd
CuongNgo-kma <ngongocbiencuong@gmail.com>
2023-06-02 21:43:46 +0700

    45d

--- src/components/mainpages/products/Products.js
+++ src/components/mainpages/products/Products.js
@@ -1,29 +1,30 @@
-import React, {useContext} from 'react'
-import {globalState} from '../../../globalState'
-import ProductItem from '../ultils/ProductItem/ProductItem';
-import Loading from '../auth/Loading/Loading';
-import Filters from './Filters';
-
+import React, { useContext } from "react";
+import { globalState } from "../../../globalState";
+import ProductItem from "../ultils/ProductItem/ProductItem";
+import Loading from "../auth/Loading/Loading";
+import Filters from "./Filters";
 
 function Products() {
-    const state = useContext(globalState)
-    const [product] = state.ProductAPI.product
-    const [isAdmin] = state.UserAPI.isAdmin 
-    return (
-        <>
-        <Filters/>
-        <div className="products">
-            {
-                product.map(product =>{
-                    return <ProductItem key={product._id} product={product} isAdmin={isAdmin}/>
-                })
-            }
-        
-
-        </div>
-        {product.length ===0 && <Loading/>}
-        </>
-    )
+  const state = useContext(globalState);
+  const [product] = state.ProductAPI.product;
+  const [isAdmin] = state.UserAPI.isAdmin;
+  return (
+    <>
+      <Filters />
+      <div className="products">
+        {product.map((product1) => {
+          return (
+            <ProductItem
+              key={product1._id}
+              product={product1}
+              isAdmin={isAdmin}
+            />
+          );
+        })}
+      </div>
+      {product.length === 0 && <Loading />}
+    </>
+  );
 }
 
-export default Products
+export default Products;
