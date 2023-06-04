8c02a1bb1e8a7c8ca938f54bef11fd85d5342eac
CuongNgo-kma <ngongocbiencuong@gmail.com>
2023-06-05 01:30:34 +0700

    .

--- src/globalState.js
+++ src/globalState.js
@@ -16,6 +16,7 @@ export const DataProvider = ({ children }) => {
       const refreshToken = async () => {
         const res = await axios.get(`/user/refresh_token`);
         setToken(res.data.accesstoken);
+        console.log(token);
         setTimeout(() => {
           refreshToken();
         }, 10 * 60 * 1000);
