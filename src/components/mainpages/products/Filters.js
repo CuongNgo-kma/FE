8739e822fc466f55d5104fe81580e8bc7e1d12ae
CuongNgo-kma <ngongocbiencuong@gmail.com>
2023-06-06 00:34:20 +0700

    .

--- src/components/mainpages/products/Filters.js
+++ src/components/mainpages/products/Filters.js
@@ -36,13 +36,13 @@ function Filters() {
 
       <div className="row">
         <span>Sort By:</span>
-        <select name="category" value={sort} onChange={e=>setSort(e.target.value)}>
+        <select name="category" value={sort} onChange={e => setSort(e.target.value)}>
           <option value="">Newest</option>
           <option value="sort=oldest">Oldest</option>
           <option value="sort=-sold">Best sales</option>
           <option value="sort=-price">Price: Hight-Low</option>
           <option value="sort=price">Price: Low-Hight</option>
-          
+
         </select>
       </div>
     </div>
